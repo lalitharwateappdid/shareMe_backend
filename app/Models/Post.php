bf3974af9de52bd60aa1c000740a5b9402c0df5d
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-11-25 12:16:47 +0530

    updated

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,4 +13,8 @@ class Post extends Model
     public function category(){
         return $this->belongsTo(Category::class,"category_id");
     }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
 }
